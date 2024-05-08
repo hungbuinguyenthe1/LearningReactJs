5a4ab799c57798eca7cd64ce73674886bc970b4a
hungbuinguyenthe1 <kayleekokoro07@gmail.com>
2024-05-08 17:01:40 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hung Bui 07102001
+          Hung Bui 0710
         </p>
         <p>
         </p>
